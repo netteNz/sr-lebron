44903c4ff51d3c777e188af216c54701f20f86eb
netteNz <lugo.emanuel@gmail.com>
2019-06-26 03:24:08 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { classPrivateMethod } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render () {
+    return (
+        <div style={{height: '300px', position: 'relative'}}>
+          <Layout style={{background: "url(https://res.cloudinary.com/dkz05ozpv/image/upload/v1561531566/background_hapn19.jpg) center / cover"}}>
+            <Header transparent title="Sr. Lebron" style={{color: 'white'}}>
+              <Navigation>
+                  <a href="/">Home</a>
+                  <a href="/">About</a>
+                  <a href="/">Contact</a>
+              </Navigation>
+          </Header>
+          <Drawer title="Sr. Lebron">
+              <Navigation>
+                  <a href="/">Explore</a>
+                  <a href="/">About</a>
+                  <a href="/">Contact</a>
+              </Navigation>
+          </Drawer>
+          <Content />
+      </Layout>
+  </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
